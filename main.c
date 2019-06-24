de211708057260bc62df38babd4f1331363d195a
neginhsobhani <neginhsobhani@gmail.com>
2019-06-24 12:52:11 +0430

    time and name condition added

--- main.c
+++ main.c
@@ -5,16 +5,23 @@
 #include <stdbool.h>
 
 struct task{
-    char taskName[100];
+    char taskName[50];
     int startTime;
     int endTime;
     struct task *next;
 };
+
 ////////function prototypes
+
 struct task* create_node();
 void add_node(struct task *taskList, struct task *new_task);
-void addTask(struct task *taskList[]);
+void addTask(struct task *week[]);
+int checkTaskTime(struct task *week[],int day,struct task *new_task);
+int checkTaskName(struct task *week[],int day, struct task *new_task);
+
+
 ////////main
+
 int main() {
     struct task *week[7];
     for(int i=0;i<7;i++){
@@ -22,7 +29,10 @@ int main() {
         week[i]->next=NULL;
 
     }
-    addTask(week);
+    while(1) {
+        printf("hello\n");
+        addTask(week);
+    }
     return 0;
 }
 struct task* create_node(){ //creates one task
@@ -48,9 +58,42 @@ void addTask(struct task *week[]) {
     printf("Enter the day\n");
     scanf("%d",&day);
     struct task *new_node = create_node();
-    add_node(week[day],new_node);
+    if(week[day]->next!=NULL) {
+        int time = checkTaskTime(week, day, new_node);
+        int name = checkTaskName(week, day, new_node);
+        if (time == 1) {
+            printf("Conflict with time\n");
+        } else if (name == 1) {
+            printf("Conflict with name\n");
+        } else {
+            add_node(week[day], new_node);
+            printf("There is no error and the new task is created\n");
+        }
+    }else{
+        add_node(week[day], new_node);
+        printf("There is no error and the new task is created\n");
+    }
 }
-
+int checkTaskTime(struct task *week[],int day,struct task *new_task){
+    struct task *current=week[day];
+        while(current!=NULL){
+            if(current->startTime < new_task->startTime < current->endTime || new_task->startTime < current->startTime < new_task->endTime){
+                return 1;
+            }
+            current=current->next;
+        }
+        return 0;
+}
+int checkTaskName(struct task *week[],int day, struct task *new_task){
+    struct task *current=week[day];
+    while(current!=NULL){
+        if(strcmp(week[day]->taskName,new_task->taskName)==0){
+            return 1;
+        }
+        current=current->next;
+    }
+    return 0;
+}
 
 
 
